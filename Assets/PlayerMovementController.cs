b86dcfdffd489c15a58c8aebfc606720121b6c63
Agustin-Q <agustin.queija@gmail.com>
2020-06-06 23:40:29 -0300

    Include player with movement

---
+++ Assets/PlayerMovementController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    CharacterController characterController;
+    public float speed = 1f;
+    private float speedDivider = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        if (characterController == null) Debug.LogError("Objet must not be null", characterController);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3();
+        move = transform.right * movementX + transform.forward * movementZ;
+        move.Normalize();
+        move = move * (speed/speedDivider) * Time.deltaTime;
+        characterController.Move(move);
+
+    }
+}
---
+++ Assets/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewController : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    private Transform playerTransform;
+    private Transform cameraTransform;
+    private float yRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+        if (playerTransform == null) Debug.LogError("Object must not be null", playerTransform);
+        cameraTransform = gameObject.transform.Find("LookCamera").transform;
+        if (cameraTransform == null) Debug.LogError("Object must not be null", cameraTransform);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float movementY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        yRotation -= movementY;
+
+        cameraTransform.localRotation = Quaternion.Euler(yRotation, 0, 0);
+        playerTransform.Rotate(Vector3.up, movementX);
+    }
+}
