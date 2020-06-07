b17435344b2dc409ef0a4636388cbf84fa50d069
Agustin-Q <agustin.queija@gmail.com>
2020-06-07 19:34:38 -0300

    PortalView working

--- Assets/Scripts/PortalViewController.cs
+++ Assets/Scripts/PortalViewController.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Esta clase es responsable de hacer que el jugador vea a traves del protal
 public class PortalViewController : MonoBehaviour
 {
+    public GameObject otherPortal;
+    public GameObject otherPortalCamera;
+    public GameObject playerCamera; 
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        //igual la configuracion de la camara del portal a la camara del jugador
+        otherPortalCamera.GetComponent<Camera>().projectionMatrix = playerCamera.GetComponent<Camera>().projectionMatrix;
+
+        //calcular direccion al protal
+        Vector3 dirToPortal = transform.position - playerCamera.transform.position;
+
+
+
+        //mover la camara del otro protal
+     
+        //obtener coordenadas de la camara del juagdor en espacio local respceto del portal
+        Vector3 PCLPosition =  transform.InverseTransformPoint(playerCamera.transform.position) ;
+        // Griar la posiciion 180 grados respexto del eje Y (esto hay que revisar si es siempre asi)
+        PCLPosition = Quaternion.Euler(0, 180, 0) * PCLPosition;
+        otherPortalCamera.transform.position = otherPortal.transform.TransformPoint( PCLPosition);
+
+        //rotar la camara al mismo angulo que la camara del jugador
+        otherPortalCamera.transform.rotation = playerCamera.transform.rotation;
+
+    }
+
+    void LogPosition(Vector3 pos, string name ="")
+    {
+        Debug.Log(name + " Position: " + pos.x + ", " + pos.y + ", " + pos.z);
     }
 }
