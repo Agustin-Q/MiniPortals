d8dfee5eaf8aae89e422c7ffb234f2527276de19
Agustin-Q <agustin.queija@gmail.com>
2020-06-11 21:01:14 -0300

    Update PortalController.cs
    Ahora puede transportar RigidBodys y conservar la cantidad de movimiento, si la clase teleprotable es de type Prop. Tambien refactor un poco del codigo

--- Assets/Scripts/PortalController.cs
+++ Assets/Scripts/PortalController.cs
@@ -9,6 +9,7 @@ public class PortalController : MonoBehaviour
     
     private PortalController otherPortalController;
     private List<GameObject> travellers = new List<GameObject>();
+    private List<GameObject> clones = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
@@ -43,34 +44,73 @@ public class PortalController : MonoBehaviour
             }
         }
     }
-
-    private void Teleport(GameObject Teleportable)
+    //pregunta: el Teleportable deberia manejar la teletransportacion?
+    private void Teleport(GameObject teleportable)
     {
         if (otherPortal != null)
         {
-            // encontrar la posicion relativa del objeto al portal en coordenadas locales
-            Vector3 relPos = transform.InverseTransformPoint(Teleportable.transform.position);
-            Debug.Log("relPos: " + relPos.ToString("F3"));
-            //girar el vector 180 grados respecto de y
-            Vector3 relPosGirado = Quaternion.Euler(0, 180, 0) * relPos; //estaba con esta linea, para calcular la nueva posicion en el otro portal
-            Debug.Log("relPosGirado: " + relPosGirado.ToString("F3"));
-            // encontrar posicion absoluta respecto de la posicion relativa al segundo portal
-            Vector3 absPos = otherPortal.transform.TransformPoint(relPosGirado);
-            Debug.Log("absPos: " + absPos.ToString("F3"));
-            // mover el objeto a la nueva posicion.
-            Teleportable.transform.position = absPos;
-            Debug.Log("Teleportable.transform.position: " + Teleportable.transform.position.ToString("F3"));
-            // calcular angulos entre los dos portales es la cantidad a rotar el judador
-            float angleBetweenPortals = otherPortal.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y ;
-            float absRotation = angleBetweenPortals + Teleportable.transform.rotation.eulerAngles.y;
-            // rotar el jugador con ese angulo. Esto no es bueno ya que solo funciona con los portales verticales, y probablemente el FPAIO tampoco funicone con el judador invertido y esta funcionalidad tiene que estar.
-            float lookAngle= Teleportable.GetComponent<FirstPersonAIO>().targetAngles.x;
-            Teleportable.GetComponent<FirstPersonAIO>().RotateCamera(new Vector2(lookAngle, absRotation),true); // esta fncion seta la orientacion en valor absoluto, no es bueno, queremos que todo sea relativo a los protales
-            
-            otherPortalController.AddTraveller(Teleportable);
+            if (teleportable.GetComponent<Teleportable>().type == "Player") {
+                // encontrar la posicion relativa del objeto al portal en coordenadas locales
+                Vector3 relPos = transform.InverseTransformPoint(teleportable.transform.position);
+                Debug.Log("relPos: " + relPos.ToString("F3"));
+                //girar el vector 180 grados respecto de y
+                Vector3 relPosGirado = Quaternion.Euler(0, 180, 0) * relPos; //estaba con esta linea, para calcular la nueva posicion en el otro portal
+                Debug.Log("relPosGirado: " + relPosGirado.ToString("F3"));
+                // encontrar posicion absoluta respecto de la posicion relativa al segundo portal
+                Vector3 absPos = otherPortal.transform.TransformPoint(relPosGirado);
+                Debug.Log("absPos: " + absPos.ToString("F3"));
+                // mover el objeto a la nueva posicion.
+                teleportable.transform.position = absPos;
+                Debug.Log("Teleportable.transform.position: " + teleportable.transform.position.ToString("F3"));
+                // calcular angulos entre los dos portales es la cantidad a rotar el judador
+                float angleBetweenPortals = otherPortal.transform.rotation.eulerAngles.y - transform.rotation.eulerAngles.y;
+                float absRotation = angleBetweenPortals + teleportable.transform.rotation.eulerAngles.y;
+                // rotar el jugador con ese angulo. Esto no es bueno ya que solo funciona con los portales verticales, y probablemente el FPAIO tampoco funicone con el judador invertido y esta funcionalidad tiene que estar.
+                float lookAngle = teleportable.GetComponent<FirstPersonAIO>().targetAngles.x;
+                teleportable.GetComponent<FirstPersonAIO>().RotateCamera(new Vector2(lookAngle, absRotation), true); // esta fncion seta la orientacion en valor absoluto, no es bueno, queremos que todo sea relativo a los protales
+            } else if (teleportable.GetComponent<Teleportable>().type == "Prop") {
+                //------Teleportar otro objeto que no sea el jugador-----
+                teleportable.transform.position = PosVectorPortalTransform(teleportable.transform.position, transform, otherPortal.transform);
+
+                teleportable.transform.rotation = RotationRelativeToPortal(teleportable.transform.rotation, transform, otherPortal.transform);
+
+                Rigidbody RB = teleportable.GetComponent<Rigidbody>();
+                RB.velocity = DirVectorPortalTransform(RB.velocity, transform, otherPortal.transform);
+
+            }
+            otherPortalController.AddTraveller(teleportable);
         }
     }
 
+    private Vector3 DirVectorPortalTransform(Vector3 vector, Transform T1, Transform T2)//esto transfroma de un portal al otro un vector;
+    {
+        Vector3 relVector = T1.InverseTransformDirection(vector);
+        relVector = Quaternion.Euler(0, 180, 0) * relVector; //rotrar 180 grados respecto de y
+        return T2.TransformDirection(relVector);
+    }
+
+    private Vector3 PosVectorPortalTransform(Vector3 vector, Transform T1, Transform T2)//esto transfroma de un portal al otro un vector;
+    {
+        // encontrar la posicion relativa del objeto al portal en coordenadas locales
+        Vector3 relPos = T1.InverseTransformPoint(vector);
+        //Debug.Log("relPos: " + relPos.ToString("F3"));
+        //girar el vector 180 grados respecto de y
+        Vector3 relPosGirado = Quaternion.Euler(0, 180, 0) * relPos; //estaba con esta linea, para calcular la nueva posicion en el otro portal
+        //Debug.Log("relPosGirado: " + relPosGirado.ToString("F3"));
+        // encontrar posicion absoluta respecto de la posicion relativa al segundo portal
+        Vector3 absPos = T2.TransformPoint(relPosGirado);
+        //Debug.Log("absPos: " + absPos.ToString("F3"));
+        return absPos;
+    }
+
+    private Quaternion RotationRelativeToPortal(Quaternion teleportable, Transform T1, Transform T2)
+    {
+        Vector3 fwrVector = teleportable * Vector3.forward;
+        Vector3 relfowarVector = T1.InverseTransformDirection(fwrVector);
+        relfowarVector = Quaternion.Euler(0, 180, 0) * relfowarVector;
+        Vector3 absForwardVector = T2.TransformDirection(relfowarVector);
+        return Quaternion.FromToRotation(fwrVector, absForwardVector);
+    }
 
     public void AddTraveller(GameObject teleportable)
     {
@@ -80,12 +120,24 @@ public class PortalController : MonoBehaviour
         }
     }
 
+    public void addClone(GameObject dummyClone)
+    {
+        if (!clones.Contains(dummyClone))
+        {
+            travellers.Add(dummyClone);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collision with: " + other.name);
-        //TODO: verificar que sea algo "teletransportable"
-        AddTraveller(other.gameObject);
-
+        //verificar que sea algo "teletransportable"
+        Teleportable teleportableObject = other.GetComponent<Teleportable>();
+        if (teleportableObject)
+        {
+            AddTraveller(other.gameObject);
+            //otherPortalController.addClone(teleportableObject.createDummyClone());
+        }
     }
 
     private void OnTriggerExit(Collider other)
