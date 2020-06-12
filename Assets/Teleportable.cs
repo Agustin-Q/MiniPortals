9c595beb079b2379a81a7741e624a463a3cc5bcb
Agustin-Q <agustin.queija@gmail.com>
2020-06-11 21:00:08 -0300

    Create Teleportable.cs

---
+++ Assets/Teleportable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//esta clase se encarga de verificar que un objeto pueda pasar por el portal, y de hacer los respectibos colones
+public class Teleportable : MonoBehaviour
+{
+    public string type = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
