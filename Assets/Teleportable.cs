d105d8519c48e18d482fcf3fb0713a9c7071ad2f
Agustin-Q <agustin.queija@gmail.com>
2020-06-13 19:24:30 -0300

    Update Teleportable.cs
    Crea un graphics clone automaticamente

--- Assets/Teleportable.cs
+++ Assets/Teleportable.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 //esta clase se encarga de verificar que un objeto pueda pasar por el portal, y de hacer los respectibos colones
 public class Teleportable : MonoBehaviour
 {
-    public string type = "";
+    public string type = "Prop";
+    private GameObject graphicsClone;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateGraphicsClone();
     }
 
     // Update is called once per frame
@@ -18,4 +19,28 @@ public class Teleportable : MonoBehaviour
     {
         
     }
+
+    // basicamente borra todo lo que no sea un meshRenederer y un MeshFilter
+    //tiene errores porque hay componentes que no se pueden borrar
+    // pero funciona igual
+    private void GenerateGraphicsClone()
+    {
+        graphicsClone = Instantiate(gameObject);
+        graphicsClone.SetActive(false);
+        Component[] components = graphicsClone.GetComponentsInChildren<Component>();
+        Debug.Log("----- components del clone -----");
+
+        for (int i = components.Length - 1; i >= 0; i--)
+        {
+            string typeString = components[i].GetType().ToString();
+            Debug.Log(typeString);
+            if (!components[i].GetType().Equals(typeof(MeshFilter)) && !components[i].GetType().Equals(typeof(MeshRenderer)) && !components[i].GetType().Equals(typeof(Transform)))
+            {
+                Destroy(components[i]);
+                Debug.Log("Destruido!!!!");
+
+            }
+        }
+    }
+   
 }
