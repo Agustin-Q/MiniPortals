79ac716be204eba47fcdaad481b4b18968a96df8
Agustin-Q <agustin.queija@gmail.com>
2020-06-13 17:38:22 -0300

    Update WeaponController.cs
    implementa una opcion para girar el proyectil

--- Assets/WeaponController.cs
+++ Assets/WeaponController.cs
@@ -7,6 +7,7 @@ public class WeaponController : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform firePoint;
     public float bulletSpeed = 1f;
+    public bool invertBullet = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,7 +29,19 @@ public class WeaponController : MonoBehaviour
 
     void Fire()
     {
-        GameObject bullet = Instantiate(bulletPrefab, firePoint.position,firePoint.rotation);
+        Quaternion bulletRot;
+        if (invertBullet)
+        {
+            bulletRot = firePoint.rotation * Quaternion.Euler(0,0,180);
+        } else
+        {
+            bulletRot = firePoint.rotation;
+        }
+
+
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, bulletRot);
+
+
         Vector3 bulletVel = new Vector3(0, 0, bulletSpeed);
         bulletVel = bullet.transform.rotation * bulletVel;
         bullet.GetComponent<Rigidbody>().velocity = bulletVel;
