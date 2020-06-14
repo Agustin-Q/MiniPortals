6579237342294467bd2726263b2a1bf4218eea0b
Agustin-Q <agustin.queija@gmail.com>
2020-06-14 20:13:04 -0300

    Agrega generacion de clones visuales

--- Assets/Teleportable.cs
+++ Assets/Teleportable.cs
@@ -7,17 +7,52 @@ using UnityEngine;
 public class Teleportable : MonoBehaviour
 {
     public string type = "Prop";
-    private GameObject graphicsClone;
+    public GameObject graphicsClone;
+    private PortalController currentPortal;
+    private PortalController otherPortal;
     // Start is called before the first frame update
     void Start()
     {
-        GenerateGraphicsClone();
+        if (graphicsClone == null)
+        {
+            GenerateGraphicsClone();
+        }
+        graphicsClone.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //si el clon esta activo
+        if (graphicsClone.activeSelf)
+        {
+            //actualziar la posicion del clon
+            graphicsClone.transform.position = transform.position;
+            graphicsClone.transform.rotation = transform.rotation;
+            graphicsClone.transform.position = currentPortal.PosVectorPortalTransform(graphicsClone.transform.position, currentPortal.transform, otherPortal.transform);
+            graphicsClone.transform.rotation = currentPortal.RotationRelativeToPortal(graphicsClone.transform.rotation, currentPortal.transform, otherPortal.transform);
+
+        }
+
+    }
+
+    public void OnPortalEnter(PortalController m_CurrentPortal, PortalController m_OtherPortal) // tecincamente no es un evento, lo llama directo el protal
+    {
+        Debug.Log("OnPortalEnter");
+        currentPortal = m_CurrentPortal;
+        otherPortal = m_OtherPortal;
+        graphicsClone.SetActive(true);
+        // activar el clon y colocarlo en la posicion correcta
+    }
+
+    public void OnPortalLeave(PortalController portal) // tecincamente no es un evento, lo llama directo el protal
+    {
+        if (currentPortal.Equals(portal)){ //igorar si estoy ya entre a orto portal
+            Debug.Log("OnPortalLeave");
+            currentPortal = null;
+            otherPortal = null;
+            graphicsClone.SetActive(false);
+        }
     }
 
     // basicamente borra todo lo que no sea un meshRenederer y un MeshFilter
